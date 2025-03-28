b499154819179dafbf3cf8fb17d6080e57718f3f
sora5042 <esuora5042@gmail.com>
2025-03-28 19:13:28 +0900

    Add Navigator

--- Chatwork/Sources/UI/Extensions/SwiftUIExtensions.swift
+++ Chatwork/Sources/UI/Extensions/SwiftUIExtensions.swift
@@ -94,6 +94,34 @@ extension View {
     }
 }
 
+extension View {
+    func navigatorDestination<Navigation: Hashable, Destination: View>(
+        _ navigation: Binding<Navigation?>,
+        @ViewBuilder destination: @escaping (Navigation) -> Destination
+    ) -> some View {
+        navigationDestination(for: Navigation.self, destination: destination)
+            .modifier(NavigationDestination(navigation: navigation))
+    }
+}
+
+private struct NavigationDestination<Navigation: Hashable>: ViewModifier {
+    @Binding
+    var navigation: Navigation?
+
+    @EnvironmentObject
+    private var navigator: ChatworkApp.Navigator
+
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: navigation) { value in
+                if let value {
+                    navigator.push(value)
+                    navigation = nil
+                }
+            }
+    }
+}
+
 // MARK: - Binding
 
 extension Binding where Value == Bool {
--- Chatwork/Sources/UI/Views/ChatworkApp.swift
+++ Chatwork/Sources/UI/Views/ChatworkApp.swift
@@ -11,9 +11,59 @@ let logger = Logger()
 
 @main
 struct ChatworkApp: App {
+    @StateObject
+    private var navigator: Navigator = .init()
+
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            NavigationStack(path: $navigator.path) {
+                switch navigator.rootView {
+                case .login:
+                    LoginView()
+                case .chatList:
+                    ChatListView()
+                }
+            }
+            .navigationViewStyle(.stack)
+            .environmentObject(navigator)
+        }
+    }
+}
+
+extension ChatworkApp {
+    @MainActor
+    final class Navigator: ObservableObject {
+        enum RootView: Int {
+            case login, chatList
+        }
+
+        @Published
+        private(set) var rootView: RootView
+
+        @Published
+        fileprivate var path: NavigationPath = .init()
+
+        init(dataStore: DataStore = SharedData.shared) {
+            rootView = dataStore.isLogin ? .chatList : .login
+        }
+
+        func switchRootView(to new: RootView) {
+            rootView = new
+        }
+
+        func popToRootView() {
+            path = .init()
+            if rootView != .login {
+                switchRootView(to: .login)
+            }
+        }
+
+        func pop() {
+            path.removeLast()
+        }
+
+        func push<Navigation: Hashable>(_ navigation: Navigation) {
+            path.append(navigation)
         }
     }
 }
